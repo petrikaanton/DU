8aa9f21acb7455fa83c0ce01975a7c9864d2279f
0knot <petrikaanton@gmail.com>
2025-09-30 19:44:09 +0200

    uloha 2

--- script.py
+++ script.py
@@ -1,9 +1,4 @@
 import math
-a=input("prve cislo: ")
+a=input("Zadaj číslo:")
 a=int(a)
-b=input("druhe cislo: ")
-b=int(b)
-if a*b<=100:
-    print("nepresiahlo 100")
-elif a*b>100:
-        print("presiahlo 100")
+print(a-a-a)
