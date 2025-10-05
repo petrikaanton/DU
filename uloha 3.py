8660d73c04165c159fcd940662ec04d0557ced4c
0knot <petrikaanton@gmail.com>
2025-10-05 11:47:30 +0200

    s

---
+++ uloha 3.py
@@ -0,0 +1,7 @@
+a=int(input("Zadaj spodnú hranicu intervalu:"))
+b=int(input("Zadaj hornú hranicu intervalu:"))
+x=int(input("Zadaj číslo:"))
+if a<=x<=b:
+    print("Čislo patrí do intervalu")
+else:
+    print("číslo nepatrí do intervalu")
